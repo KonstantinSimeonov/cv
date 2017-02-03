3e646ff1f3577da9c59f1ef52b1bd77e4ce214b7
Konstantin Simeonov <kon.simeonov@gmail.com>
2017-02-03 23:39:04 +0200

    Writing css is like chopping onions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,8 +9,23 @@ $(() => {
     ])
     .then(resolved => {
         const [skillsTemplate, workExpTemplate, personalInfoTemplate, personalData] = resolved;
-        console.log(personalData);
-        $('.skills-section').html(skillsTemplate(personalData));
+        
+        const skills = [
+            [1, 2, 3, 4],
+            [1, 2, 3, 4],
+            [1, 2, 3, 4],
+            [1, 2]
+        ];
+        
+        let next = 0;
+        for(let i = 0, len = skills.length; i < len; i += 1) {
+            for(let j = 0, len2 = skills[i].length; j < len2; j += 1) {
+                skills[i][j] = personalData.skills[next++];
+            }
+        }
+
+        $('.skills-section').html(skillsTemplate({ rows: skills }));
+
         $('.work-expirience-section').html(workExpTemplate(personalData));
         $('.personal-info-panel').html(personalInfoTemplate(personalData.personalInfo));
 })
