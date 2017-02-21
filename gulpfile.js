28be466abaf75964581c598f89f5066cef1655e2
Konstantin Simeonov <kon.simeonov@gmail.com>
2017-02-21 18:13:49 +0200

    Goodbye bash script

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,31 @@ const gulp = require('gulp'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
     babel = require('gulp-babel'),
-    minifyCss = require('gulp-clean-css');
+    minifyCss = require('gulp-clean-css'),
+    gulpCopy = require('gulp-copy'),
+    gulpClean = require('gulp-clean');
+
+const BUILD_DIR = './build';
+
+gulp.task('clean', () => gulp.src(BUILD_DIR, { read: false }).pipe(gulpClean()));
 
 gulp.task('bundle', () => gulp
-                            .src('./src/*.html')
-                            .pipe(useref())
-                            .pipe(gulpif('*.js', babel({ presets: ['es2015'] })))
-                            .pipe(gulpif('*.js', uglify()))
-                            .pipe(gulpif('*.css', minifyCss()))
-                            .pipe(gulp.dest('../build')));
+    .src('./src/*.html')
+    .pipe(useref())
+    .pipe(gulpif('*.js', babel({ presets: ['es2015'] })))
+    .pipe(gulpif('*.js', uglify()))
+    .pipe(gulpif('*.css', minifyCss()))
+    .pipe(gulp.dest(BUILD_DIR)));
+
+gulp.task('copy', () => gulp.src([
+    './src/package.json',
+    './src/app.js',
+    './src/resume.pdf',
+    './src/data/data.json',
+    './src/public/assets/*',
+    './src/public/templates/*.handlebars',
+    './src/public/components/handlebars/handlebars.min.js',
+    './src/public/components/jquery/dist/jquery.min.js'
+]).pipe(gulpCopy(BUILD_DIR, { prefix: 1 })));
+
+gulp.task('default', ['bundle', 'copy']);
