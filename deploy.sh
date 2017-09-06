408ea266ff049d8dcd87570b39d7a594299286ad
Konstantin Simeonov <kon.simeonov@protonmail.com>
2017-09-06 16:58:47 +0300

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,12 @@
-#! /usr/bin/bash
+#! /bin/bash
 
-BUILD_DIR="../cv-build"
+BUILD_DIR="./build"
 LAST_COMMIT_MESSAGE=$(git log -1 --pretty=%B)
 
-npm run build
-rm -rf "$BUILD_DIR"
-mv build "$BUILD_DIR"
+yarn build:prod
 cd "$BUILD_DIR"
 git init
 git add .
 git commit -m "$LAST_COMMIT_MESSAGE"
-heroku git:remote -a simeonov-resume
+~/.node_modules/bin/heroku git:remote -a simeonov-resume
 git push -f heroku master
