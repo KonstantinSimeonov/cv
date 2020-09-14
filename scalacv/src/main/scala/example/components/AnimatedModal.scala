11dee3ba0faac10a0691ac709ab6d6b7013dabe8
Konstantin Simeonov <kon.simeonov@protonmail.com>
2020-09-15 02:51:08 +0300

    Fix dem animations

--- scalacv/src/main/scala/example/components/AnimatedModal.scala
+++ scalacv/src/main/scala/example/components/AnimatedModal.scala
@@ -1,6 +1,7 @@
 package example.components
 
 import org.scalajs.dom._
+import scala.scalajs.js.timers._
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
 
@@ -11,25 +12,46 @@ object modal_window {
     content: String = "",
     onClose: Callback = Callback {}
   )
-  val ModalWindow =
-    ScalaComponent.builder[ModalWindowProps]
-      .render_P { props =>
-        val cls = if (props.open) "in" else ""
-        <.div(^.className := s"$cls gray-overlay",
-          <.div(^.className := s"$cls skill-details-container",
-            <.a(
-              ^.className := "close-btn",
-              ^.onClick --> props.onClose,
-              "close"
-            ),
-            <.article(^.className := "skill-details",
-              <.strong(^.className := "skill-title", props.title),
-              <.p(
-                ^.className := "skill-description",
-                ^.dangerouslySetInnerHtml := props.content
-              )
+
+  class ModalWindowBackend($: BackendScope[ModalWindowProps, Boolean]) {
+    def render(props: ModalWindowProps, in: Boolean): VdomNode = {
+      val cls = if (in) "in" else ""
+      val open = if (props.open) "open" else ""
+      <.div(
+        ^.classSet(
+          "gray-overlay" -> true,
+          "in" -> in,
+          "open" -> props.open
+        ),
+        <.div(
+          ^.classSet(
+            "skill-details-container" -> true,
+            "in" -> in
+          ),
+          <.a(
+            ^.className := "close-btn",
+            ^.onClick --> props.onClose,
+            "close"
+          ),
+          <.article(^.className := "skill-details",
+            <.strong(^.className := "skill-title", props.title),
+            <.p(
+              ^.className := "skill-description",
+              ^.dangerouslySetInnerHtml := props.content
             )
           )
         )
-      }.build
+      )
+    }
+  }
+
+  val ModalWindow =
+    ScalaComponent.builder[ModalWindowProps]
+      .initialState(false)
+      .renderBackend[ModalWindowBackend]
+      .componentWillReceiveProps(rp => Callback {
+        if (rp.currentProps.open != rp.nextProps.open)
+          setTimeout(200) { rp.setState(rp.nextProps.open).runNow() }
+      })
+      .build
 }
