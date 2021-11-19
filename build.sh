fcfd908f3949ce907271f7e9f1c39a7a4eed7b17
Konstantin Simeonov <kon.simeonov@protonmail.com>
2021-11-19 19:20:19 +0200

    2Pac - Changes

--- build.sh
+++ build.sh
@@ -13,5 +13,5 @@ cp "target/$ASSETS_DIR/"*.{png,jpg,gif,json,pdf} "$BUILD_DIR/target/$ASSETS_DIR"
 cp "target/$JS_DIR/scalacv-opt-bundle.js" "$BUILD_DIR/target/$JS_DIR/scalacv-fastopt-bundle.js"
 echo > build/requirements.txt
 
-env GOOS=linux GOARCH=amd64 go build web.go
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build web.go
 cp web package.json Procfile "$BUILD_DIR"
