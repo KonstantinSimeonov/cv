8c6ce0a2b40ff4e06f329347b44b40cfad90154f
Konstantin Simeonov <kon.simeonov@protonmail.com>
2021-11-28 00:00:49 +0200

    Add the favorite kon and some asset optimizations

--- build.sh
+++ build.sh
@@ -8,8 +8,23 @@ rm -rf build
 sbt 'assets; webPipeline; fullOptJS::webpack'
 mkdir -p $BUILD_DIR/target/{$ASSETS_DIR,$JS_DIR}
 cp index.html $BUILD_DIR
+
+# optimizations
+
+# shrink bigger pngs
+for img in "target/$ASSETS_DIR"/*.png; do
+    convert "$img" -resize 64x64\> "$img"
+done
+
+function opt_json() {
+    node -p 'JSON.stringify(require(`'"./target/$ASSETS_DIR/data.json"'`))'
+}
+
+
+cp favicon.png "$BUILD_DIR/favicon.ico"
 cp "target/$ASSETS_DIR/styles.css" "$BUILD_DIR/target/$ASSETS_DIR"
-cp "target/$ASSETS_DIR/"*.{png,jpg,gif,json,pdf} "$BUILD_DIR/target/$ASSETS_DIR"
+cp "target/$ASSETS_DIR/"*.{png,jpg,gif,pdf} "$BUILD_DIR/target/$ASSETS_DIR"
+opt_json > "$BUILD_DIR/target/$ASSETS_DIR/data.json"
 cp "target/$JS_DIR/scalacv-opt-bundle.js" "$BUILD_DIR/target/$JS_DIR/scalacv-fastopt-bundle.js"
 echo > build/requirements.txt
 
